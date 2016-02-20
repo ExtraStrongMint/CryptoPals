d17c2febf118429749981b676602b3d1c1b44fa8
ExtraStrongMint <admin@extrastrongmint.com>
2016-02-20 10:35:41 +0000

    Added challenge set 1

--- CryptoPals/Modules/Basics.cs
+++ CryptoPals/Modules/Basics.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace CryptoPals.Modules
 {
-    class Basics
+    public static class Basics
     {
+        public static string Solve(int challenge)
+        {
+            switch (challenge)
+            {
+                case 1:
+                    return One();
+                case 2:
+                    return Two();
+                case 3:
+                    return Three();
+                case 4:
+                    return Four();
+                case 5:
+                    return Five();
+                case 6:
+                    return Six();
+                case 7:
+                    return Seven();
+                case 8:
+                    return Eight();
+                default:
+                    return "Unknown";
+            }
+        }
+
+        private static string One()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string Two()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string Three()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string Four()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string Five()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string Six()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string Seven()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string Eight()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CryptoPals/Utilities.cs
+++ CryptoPals/Utilities.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace CryptoPals
 {
-    public class Utilities
+    public static class Utilities
     {
+        #region Byte Conversions
+        public static byte[] HexStringToBytes(this string hex_string)
+        {
+            StringBuilder _ret = new StringBuilder();
+            for (int i = 0; i < hex_string.Length; i += 2)
+            {
+                string _char = hex_string.Substring(i, 2);
+                _ret.Append(Convert.ToChar(Convert.ToUInt32(_char, 16)));
+            }
+
+            return Encoding.ASCII.GetBytes(_ret.ToString());
+        }
+        #endregion
     }
 }
